48cdac23425c3beadb199a5093de7705efb0b0c2
Lorenzo Ciani <l.ciani@xister.com>
2022-12-08 20:12:05 +0100

    Hello there
    aaaa

--- thrusters/engine1/index.js
+++ thrusters/engine1/index.js
@@ -1,19 +1,22 @@
 const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
 
 exports.handler = async (event) => {
+    let number1 = JSON.parse(event.body).number1
+    let number2 = JSON.parse(event.body).number2
     let input = {
         QueueUrl: process.env.FIFOURL,
         MessageGroupId: "group1",
         MessageDeduplicationId: Math.random(),
         MessageBody: {
-            number1: JSON.parse(event.body).number1,
-            number2: JSON.parse(event.body).number2,
+            number1: number1,
+            number2: number2,
             ip: event.requestContext.http.sourceIp,
             timestamp: Date()
         }
     }
     const client = new SQSClient({ region: process.env.REGION })
     const command = new SendMessageCommand(input)
+    let sum = number1 + number2
     try {
         const response = await client.send(command)
         return {
@@ -23,6 +26,5 @@ exports.handler = async (event) => {
         }
     } catch (error) {
         return error
-
     }
 }
