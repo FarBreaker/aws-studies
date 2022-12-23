3e5f346377ae8f964029b9c70eec3ede0bae7c60
Lorenzo Ciani <l.ciani@xister.com>
2022-12-23 18:47:02 +0100

    Added Dynamo structure to write an item, message reading still KO

--- thrusters/engine1/index.js
+++ thrusters/engine1/index.js
@@ -4,27 +4,15 @@ export async function handler(event) {
     let number1 = JSON.parse(event.body).number1
     let number2 = JSON.parse(event.body).number2
     let sum = number1 + number2
-    // let input = {
-    //     QueueUrl: process.env.FIFOURL,
-    //     MessageGroupId: "group1",
-    //     MessageDeduplicationId: Math.random(),
-    //     MessageBody: JSON.stringify({
-    //         number1: number1,
-    //         number2: number2,
-    //         sum: sum,
-    //         ip: event.requestContext.http.sourceIp,
-    //         timestamp: Date()
-    //     })
+    // function MakeId(length) {
+    //     let result = '';
+    //     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    //     let charactersLength = characters.length;
+    //     for (let i = 0; i < length; i++) {
+    //         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    //     }
+    //     return result;
     // }
-    function MakeId(length) {
-        let result = '';
-        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    }
     const params = {
         MessageAttributes: {
             Title: {
@@ -40,13 +28,13 @@ export async function handler(event) {
             timestamp: Date()
         }),
         QueueUrl: process.env.FIFOURL,
-        MessageDeduplicationId: MakeId(10),
-        MessageGroupId: "Group1"
+        DelaySeconds: 4,
+        VisibilityTimeout: 60
     }
     const client = new SQSClient({ region: process.env.REGION })
     try {
         const data = await client.send(new SendMessageCommand(params))
-        console.log("Success, message sent. MessageID: ", data.MessageId)
+        console.log("Success, message sent. MessageID: ", data)
         return {
             statusCode: 200,
             headers: { "content-type": "application/json" },
@@ -55,6 +43,4 @@ export async function handler(event) {
     } catch (err) {
         return err.toString()
     }
-
-
 }
--- thrusters/engine2/index.js
+++ thrusters/engine2/index.js
@@ -1,8 +1,9 @@
 import { ReceiveMessageCommand, DeleteMessageCommand, SQSClient } from "@aws-sdk/client-sqs"
-const client = new SQSClient({ region: process.env.REGION })
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+const sqsClient = new SQSClient({ region: process.env.REGION })
+const dynamoClient = new DynamoDBClient({ region: process.env.REGION })
 const queueUrl = process.env.FIFOURL;
-const params = {
-    AttributeNames: ["SentTimestamp"],
+const sqsParams = {
     MaxNumberOfMessages: 10,
     MessagesAttributeNames: ["All"],
     QueueUrl: queueUrl,
@@ -10,22 +11,46 @@ const params = {
     WaitTimeSeconds: 0
 }
 
+function MakeId(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 export async function handler() {
+    let dynamoParams = {
+        TableName: process.env.DATABASE_TABLE,
+        Item: {
+            ID: { S: MakeId(16) },
+            timestamp: { S: Date() },
+            status: { S: "Failed" }
+        }
+    }
     try {
-        const data = await client.send(new ReceiveMessageCommand(params));
+        const data = await sqsClient.send(new ReceiveMessageCommand(sqsParams));
         if (data.Messages) {
+            dynamoParams = { ...dynamoParams, status: { S: "Success" } }
             let deleteParams = {
                 QueueUrl: queueUrl,
                 ReceiptHandle: data.Messages[0].ReceiptHandle
             }
             try {
-                const data = await client.send(new DeleteMessageCommand(deleteParams))
+                const data = await sqsClient.send(new DeleteMessageCommand(deleteParams))
                 console.log("Message deleted", data)
             } catch (err) {
                 console.log("Error", err)
             }
         } else {
             console.log("No messages to delete")
+        } try {
+            const data = await dynamoClient.send(new PutItemCommand(dynamoParams))
+            console.log("Item inserted correctly ", data)
+        } catch (err) {
+            console.log("Error ", err)
         }
         return data
     } catch (err) {
